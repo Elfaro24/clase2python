e71ba92adc116e7cc944c4012b43785900f3fb26
cbdiaz <carboad@gmail.com>
2024-05-18 15:49:41 -0400

    prueba de condicional

---
+++ condicionales/if-anidados.py
@@ -0,0 +1,16 @@
+dias = "Lunes"
+
+estas_enfermo = True
+
+if dias == "Lunes":
+    print ("Hoy voy al colegio")
+
+    if estas_enfermo == False:
+
+        print("No estoy enfermo,voy a poder ir al colegio")
+
+    else:
+        print("Pero no ire al coegio porque no estoy enfermo")
+
+else:
+    print("Hoy no es lunes, no hay clases")
